a97f4ec330557ead47af12f07bdc7e8011481bb1
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-06-28 19:19:37 +0530

    main and home page initializing

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,9 +12,60 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.only(top: 50 , left: 20),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            const Color.fromARGB(255, 217, 214, 228),
+            const Color.fromARGB(255, 178, 185, 183),
+            const Color.fromARGB(255, 255, 255, 255),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight, ),
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            
+            Row(  // icon and the location
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.location_on_outlined),
+                Text('Ratnapura' , style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text('Hello , Sandinu' , style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),),
+            SizedBox(height: 8),
+            Text('There are 20 events\naround your location' , style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                //pading: EdgeInsets.only(left: 20),
+                decoration: InputDecoration(
+                  hintText: 'Search for events',
+                  prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                ),
+              ),
+            )
           ],
         ),
       ),
