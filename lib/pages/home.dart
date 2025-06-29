c13005569a09cb272c707e0182dd2b39097233b0
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-06-29 10:54:38 +0530

    adding event list and upcoming events

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -28,7 +28,6 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(  // icon and the location
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Icon(Icons.location_on_outlined),
                 Text('Ratnapura' , style: TextStyle(
@@ -50,7 +49,11 @@ class _HomeState extends State<Home> {
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
                 ),),
+            SizedBox(height: 20),
+
+            // Search bar
             Container(
+              margin: EdgeInsets.only(right: 20),
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -64,6 +67,119 @@ class _HomeState extends State<Home> {
                   border: InputBorder.none,
                   contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                 ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 80,
+              child: ListView( // to show categories
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: 130,
+                        height: 130,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: Colors.white ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/musical.png', height: 30, width: 30,fit: BoxFit.cover,),
+                            Text('Music' , style: TextStyle(color: Colors.black , fontSize: 20),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: 130,
+                        height: 130,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: Colors.white ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/tshirt.png', height: 30, width: 30,fit: BoxFit.cover,),
+                            Text('Clothing' , style: TextStyle(color: Colors.black , fontSize: 20),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: 130,
+                        height: 130,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: Colors.white ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/confetti.png', height: 30, width: 30,fit: BoxFit.cover,),
+                            Text('Festival' , style: TextStyle(color: Colors.black , fontSize: 20),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Material(
+                      elevation: 5,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: 130,
+                        height: 130,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(color: Colors.white ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/dish.png', height: 30, width: 30,fit: BoxFit.cover,),
+                            Text('Food' , style: TextStyle(color: Colors.black , fontSize: 20),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Upcoming Events' , style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                ],
+              ),
+            ),
+            Container(
+              
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
               ),
             )
           ],
