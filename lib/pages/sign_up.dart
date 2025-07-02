d312f640e8c8569a4d201a0ead6a19f5b771b4a0
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-07-02 14:43:29 +0530

    signing page and firebase project creation

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -15,7 +15,60 @@ class _SignUpState extends State<SignUp> {
       body: Container(
         child: Column(
           children: [
-            
+            Image.asset('assets/images/onboarding.png',
+            // fit: BoxFit.cover,
+            ),
+            SizedBox(height: 12),
+            Text("Welcome to Event Management System",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text("Let's see your events",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 98, 12, 210),
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text("Discover and manage events effortlessly",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 45),
+            Container(
+              height: 60,
+              margin: EdgeInsets.only(left: 32, right: 32),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 98, 12, 210),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/google.png',
+                    height: 30,
+                    width: 30,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(width: 10),
+                  Text("Sign up with Google",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
