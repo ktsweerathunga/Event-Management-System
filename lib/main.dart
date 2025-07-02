a87d6197ef1442ea4721e8526d4e42b28b68bd9e
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-07-03 00:33:44 +0530

    adding firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:event_management_system/components/widgets/bottum_nav.dart';
 import 'package:event_management_system/pages/home.dart';
 import 'package:event_management_system/pages/sign_up.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // Initialize any necessary services or plugins here
   runApp(const MyApp());
 }
 
