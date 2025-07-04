214aa6c323894f97274cd3bd9a15efad984982e4
ktsweerathunga <135198812+ktsweerathunga@users.noreply.github.com>
2025-07-04 23:32:33 +0530

    Add buttons and uploading options

--- lib/admin/upload_event.dart
+++ lib/admin/upload_event.dart
@@ -104,6 +104,14 @@ class _UploadEventState extends State<UploadEvent> {
                 ),
               ),
             ),
+            SizedBox(height: 30,),
+            Text('Select Event Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
             SizedBox(height: 10,),
             Container(
               width: MediaQuery.of(context).size.width,
@@ -111,28 +119,106 @@ class _UploadEventState extends State<UploadEvent> {
               decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(10),
               ),
-              child: DropdownButtonFormField<String>(
+              child: DropdownButtonHideUnderline(
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Select Event Type',
+                  ),
+                  value: _selectedEventType,
+                  items: _eventTypes.map((String eventType) {
+                    return DropdownMenuItem<String>(
+                      value: eventType,
+                      child: Text(eventType,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedEventType = newValue;
+                    });
+                  },
+                  dropdownColor: Colors.white,
+                  icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                  hint: Text(
+                    'Select Event Type',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  iconSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text('Event Detail',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            SizedBox(height: 10,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                maxLines: 6,
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  hintText: 'Select Event Type',
+                  hintText: 'Describe the event details here',
                 ),
-                value: _selectedEventType,
-                items: _eventTypes.map((String eventType) {
-                  return DropdownMenuItem<String>(
-                    value: eventType,
-                    child: Text(eventType,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
+              ),
+            ),
+            // Container(decoration: BoxDecoration(
+            //   color: const Color.fromARGB(255, 98, 12, 210),
+            //   borderRadius: BorderRadius.circular(10),
+            // ),child: ElevatedButton(
+            //   onPressed: () {
+            //     // Handle upload event logic here
+            //     ScaffoldMessenger.of(context).showSnackBar(
+            //       SnackBar(
+            //         content: Text('Event Uploaded Successfully!'),
+            //         backgroundColor: Colors.green,
+            //       ),
+            //     );
+            //   },
+            //   style: ElevatedButton.styleFrom(
+            //     backgroundColor: const Color.fromARGB(255, 98, 12, 210),
+            //     padding: EdgeInsets.symmetric(vertical: 15),
+            //   ),
+            //   child: Text(
+            //     'Upload Event',
+            //     style: TextStyle(fontSize: 18, color: Colors.white),
+            //   ),
+            // ),
+            // ),
+            Center(
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color:  Color.fromARGB(255, 98, 12, 210),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Upload',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedEventType = newValue;
-                  });
-                },
+                  ),
+                ),
               ),
             )
           ],
